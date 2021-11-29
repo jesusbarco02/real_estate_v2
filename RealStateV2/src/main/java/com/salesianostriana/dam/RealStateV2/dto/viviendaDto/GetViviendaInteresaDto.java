6fd02f40e9b0ef50a93e93625343172b89532091
Jesús <74898703+jesusbarco02@users.noreply.github.com>
2021-11-29 23:38:09 +0100

    Consulta apartado 2

---
+++ RealStateV2/src/main/java/com/salesianostriana/dam/RealStateV2/dto/viviendaDto/GetViviendaInteresaDto.java
@@ -0,0 +1,23 @@
+package com.salesianostriana.dam.RealStateV2.dto.viviendaDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GetViviendaInteresaDto {
+
+    private Long id;
+    private String titulo;
+    private String provincia;
+    private int numBanios;
+    private int numHabitaciones;
+    private double metrosCuadrados;
+    private double precio;
+    private String descripcion;
+    private String avatar;
+    private boolean interesa;
+}
--- RealStateV2/src/main/java/com/salesianostriana/dam/RealStateV2/repos/ViviendaRepository.java
+++ RealStateV2/src/main/java/com/salesianostriana/dam/RealStateV2/repos/ViviendaRepository.java
@@ -2,6 +2,7 @@ package com.salesianostriana.dam.RealStateV2.repos;
 
 
 import com.salesianostriana.dam.RealStateV2.dto.viviendaDto.GetViviendaDto;
+import com.salesianostriana.dam.RealStateV2.dto.viviendaDto.GetViviendaInteresaDto;
 import com.salesianostriana.dam.RealStateV2.dto.viviendaDto.GetViviendaPropietarioDto;
 import com.salesianostriana.dam.RealStateV2.model.Vivienda;
 import com.salesianostriana.dam.RealStateV2.usuarios.model.Usuario;
@@ -31,6 +32,13 @@ public interface ViviendaRepository extends JpaRepository<Vivienda, Long> {
             from Vivienda v where v.usuario.id = :id
             """)
     List<GetViviendaDto> todosLasViviendasPropietario(Long id);
+    @Query("""
+            select new com.salesianostriana.dam.RealStateV2.dto.viviendaDto.GetViviendaInteresaDto(
+                v.id, v.titulo, v.provincia, v.numBanios, v.numHabitaciones, v.metrosCuadrados, v.precio, v.descripcion, v.avatar
+            )
+            from Vivienda v where v.usuario.id = :id
+            """)
+    List<GetViviendaInteresaDto> todosLasViviendasInteresado(Long id);
 
 
 
